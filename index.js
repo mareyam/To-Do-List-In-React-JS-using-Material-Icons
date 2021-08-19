427c2e5b809aae078e6e9d3d308330e8067ee64a
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2021-08-19 21:36:08 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import index from './index.css';
+import ToDoList from './ToDoList';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
